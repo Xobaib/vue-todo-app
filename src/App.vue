5197dcba836eeb7d50718327c5a8c8dd85ea5a31
xobaib <xobaib2001.m@gmail.com>
2024-12-12 21:09:38 +0330

    Navigation Component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import TodoHeader from './components/TodoHeader.vue';
 </script>
 
 
 <template>
+  <TodoHeader />
   <RouterView />
 </template>
 
